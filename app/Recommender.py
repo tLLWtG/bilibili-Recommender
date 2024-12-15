f33ae6840d56a26c0d693d7cdf60303d2c651019
tllwtg <tllwtg@gmail.com>
2024-12-15 22:18:20 +0800

    *

--- app/Recommender.py
+++ app/Recommender.py
@@ -1,20 +1,21 @@
-'''
+"""
 Author: wlaten
 Date: 2024-12-15 13:56:25
 LastEditTime: 2024-12-15 22:04:41
 Discription: file content
-'''
+"""
 
-from getHistoryData import get_history_data
-from getHotData import get_hot_data
-from getRecommandData import get_recommand_data
+from app.getHistoryData import get_history_data
+from app.getHotData import get_hot_data
+from app.getRecommandData import get_recommand_data
 
-from model import *
+from app.model import *
+
 
 class Recommender:
-    def __init__(self):
-        pass
-    
+    # def __init__(self):
+    #     pass
+
     def __init__(self, cookies):
         """
         初始化，仅需要传入cookies
@@ -23,67 +24,84 @@ class Recommender:
         self.cookies = cookies
         self.video_pool_hot = []
         self.video_pool_recommend = []
-        
+
         self.processor = FeatureProcessor()
-        self.processed_data, self.labels, self.max_tags = load_and_process_data("historyVideo.json", self.processor)
+        self.processed_data, self.labels, self.max_tags = load_and_process_data(
+            "historyVideo.json", self.processor
+        )
         # self.processed_data, self.labels, self.max_tags = load_and_process_data(None, self.processor, cookies)
         self.class_weights_array = class_weight.compute_class_weight(
-            class_weight='balanced',
-            classes=np.unique(self.labels),
-            y=self.labels
+            class_weight="balanced", classes=np.unique(self.labels), y=self.labels
         )
-        self.class_weights_dict = {i: weight for i, weight in enumerate(self.class_weights_array)}
+        self.class_weights_dict = {
+            i: weight for i, weight in enumerate(self.class_weights_array)
+        }
         print(f"类别权重: {self.class_weights_dict}")
-        
+
         self.num_tags = len(self.processor.tag2idx)
         self.num_authors = len(self.processor.author2idx)
         self.embedding_dim = 32
-        
-        self.model = VideoRecommender(self.num_tags, self.num_authors, self.embedding_dim)
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        
+
+        self.model = VideoRecommender(
+            self.num_tags, self.num_authors, self.embedding_dim
+        )
+        self.optimizer = keras.optimizers.Adam(learning_rate=0.001)
+
         num_epochs = 30
-        
-        best_loss = float('inf')
+
+        best_loss = float("inf")
         losses = []
         AUCROC = []
         AveragePrecision = []
         PrecisionAtK = []
         RecallAtK = []
-    
+
         for epoch in range(num_epochs):
             print(f"\nEpoch {epoch + 1}/{num_epochs}")
-            
-            loss = train_model(self.model, self.processed_data, self.labels, self.optimizer, self.class_weights_dict)
+
+            loss = train_model(
+                self.model,
+                self.processed_data,
+                self.labels,
+                self.optimizer,
+                self.class_weights_dict,
+            )
             losses.append(loss)
             print(f"Epoch {epoch + 1}, Average Loss: {loss}")
-            
+
             metrics = evaluate_model(self.model, self.processed_data, self.labels)
             print(f"评估指标: {metrics}")
-            
-            AUCROC.append(metrics['AUC-ROC'])
-            AveragePrecision.append(metrics['Average Precision'])
-            PrecisionAtK.append(metrics['Precision@k'])
-            RecallAtK.append(metrics['Recall@k'])
-            
+
+            AUCROC.append(metrics["AUC-ROC"])
+            AveragePrecision.append(metrics["Average Precision"])
+            PrecisionAtK.append(metrics["Precision@k"])
+            RecallAtK.append(metrics["Recall@k"])
+
             if loss < best_loss:
                 best_loss = loss
                 print(f"在 epoch {epoch + 1} 保存最佳模型")
                 save_model_and_processor(self.model, self.processor, save_dir)
-                
+
             plt.figure(figsize=(10, 6))
-            plt.plot(range(1, len(losses)+1), losses, 'b-', label='Training Loss')
-            plt.plot(range(1, len(AUCROC)+1), AUCROC, 'r-', label='AUC-ROC')
-            plt.plot(range(1, len(AveragePrecision)+1), AveragePrecision, 'g-', label='Average Precision')
-            plt.plot(range(1, len(PrecisionAtK)+1), PrecisionAtK, 'y-', label='Precision@k')
-            plt.plot(range(1, len(RecallAtK)+1), RecallAtK, 'c-', label='Recall@k')
-            
-            plt.title('Training Metrics')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
+            plt.plot(range(1, len(losses) + 1), losses, "b-", label="Training Loss")
+            plt.plot(range(1, len(AUCROC) + 1), AUCROC, "r-", label="AUC-ROC")
+            plt.plot(
+                range(1, len(AveragePrecision) + 1),
+                AveragePrecision,
+                "g-",
+                label="Average Precision",
+            )
+            plt.plot(
+                range(1, len(PrecisionAtK) + 1), PrecisionAtK, "y-", label="Precision@k"
+            )
+            plt.plot(range(1, len(RecallAtK) + 1), RecallAtK, "c-", label="Recall@k")
+
+            plt.title("Training Metrics")
+            plt.xlabel("Epoch")
+            plt.ylabel("Loss")
             plt.legend()
             plt.grid(True)
-            plt.savefig(os.path.join(save_dir, 'training_loss.png'))
+            plt.savefig(os.path.join(save_dir, "training_loss.png"))
             plt.close()
 
     def behavior_insert(self, video):
@@ -111,97 +129,100 @@ class Recommender:
         }
         """
         pass
-    
+
     def expand_pool(self, pool_type, video_cnt):
         """
         从热门或推荐视频中扩充推荐池
         pool_type: 推荐池类型，hot或者recommend
         """
-        if pool_type == 'hot':
+        if pool_type == "hot":
             data = get_hot_data(self.cookies, video_cnt)
             self.video_pool_hot += data
-        elif pool_type == 'recommend':
+        elif pool_type == "recommend":
             data = get_recommand_data(self.cookies, video_cnt)
             self.video_pool_recommend += data
-    
-    def recommend(self, pool_type, video_cnt = 1):
+
+    def recommend(self, pool_type, video_cnt=1):
         """
         返回推荐池中最推荐的视频，返回的是一个视频列表
         返回后会pop掉这些视频
         如果视频不够会自己添加
         pool_type: 推荐池类型，hot或者recommend
         """
-        pool = self.video_pool_hot if pool_type == 'hot' else self.video_pool_recommend
-        
-        if (video_cnt > len(pool)):
+        pool = self.video_pool_hot if pool_type == "hot" else self.video_pool_recommend
+
+        if video_cnt > len(pool):
             self.expand_pool(pool_type, video_cnt * 3 - len(pool))
-        
+
         processed_videos = []
         for video in pool:
-            valid_tags = [tag for tag in video['tag'] if tag in self.processor.tag2idx]
+            valid_tags = [tag for tag in video["tag"] if tag in self.processor.tag2idx]
             if not valid_tags:
                 continue
-            processed_videos.append({
-                **video,
-                'tag': valid_tags
-            })
-        
+            processed_videos.append({**video, "tag": valid_tags})
+
         print(f"找到 {len(processed_videos)} 个有效视频")
         if not processed_videos:
             return []
-        
+
         video_info = []
         all_tags = []
         all_authors = []
         all_quality_scores = []
 
         for video in processed_videos:
-            tags = [self.processor.tag2idx[tag] for tag in video['tag']]
+            tags = [self.processor.tag2idx[tag] for tag in video["tag"]]
             tags = tags + [0] * (self.max_tags - len(tags))
-            
-            author_idx = self.processor.author2idx[video['author']] if video['author'] in self.processor.author2idx else 0
-            
-            quality_score = self.processor.calculate_quality_score(
-                video['view'],
-                video['like'],
-                video['favorite']
+
+            author_idx = (
+                self.processor.author2idx[video["author"]]
+                if video["author"] in self.processor.author2idx
+                else 0
             )
-            
+
+            quality_score = self.processor.calculate_quality_score(
+                video["view"], video["like"], video["favorite"]
+            )
+
             all_tags.append(tags)
             all_authors.append(author_idx)
             all_quality_scores.append(quality_score)
-            
+
             video_info.append(video)
         processed_data = {
-            'tags': np.array(all_tags, dtype=np.int32),
-            'author': np.array(all_authors, dtype=np.int32),
-            'quality_score': np.array(all_quality_scores, dtype=np.float32)
+            "tags": np.array(all_tags, dtype=np.int32),
+            "author": np.array(all_authors, dtype=np.int32),
+            "quality_score": np.array(all_quality_scores, dtype=np.float32),
         }
-        predictions = self.model([
-            processed_data['tags'],
-            processed_data['author'],
-            processed_data['quality_score']
-        ]).numpy().flatten()
-        
+        predictions = (
+            self.model(
+                [
+                    processed_data["tags"],
+                    processed_data["author"],
+                    processed_data["quality_score"],
+                ]
+            )
+            .numpy()
+            .flatten()
+        )
+
         results = []
         for i, pred in enumerate(predictions):
-            results.append({
-                **video_info[i],
-                'rating': float(pred)
-            })
-        
-        results.sort(key=lambda x: x['rating'], reverse=True)
-        
+            results.append({**video_info[i], "rating": float(pred)})
+
+        results.sort(key=lambda x: x["rating"], reverse=True)
+
         top_videos = results[:video_cnt]
         # for video in top_videos:
-        #     print(video) 
+        #     print(video)
 
         return top_videos
-    
+
+
 if __name__ == "__main__":
     cookie_path = os.path.join("user_data", "cookie.txt")
     with open(cookie_path, "r", encoding="utf-8") as file:
         debug_cookies = file.read().strip()
-        
+
     recommender = Recommender(debug_cookies)
-    print(recommender.recommend("hot",5))
+    print(recommender.recommend("hot", 5))
--- app/getHotData.py
+++ app/getHotData.py
@@ -1,32 +1,31 @@
 import requests
-import selenium
 import json
 
 # len为需要读取的信息量（有可能会小于len,因为可能总共的信息就不多）
+
 
 def get_hot_data(cookie, len):
     # 目标 URL
     url = "https://api.bilibili.com/x/web-interface/popular/series/list"
     headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Cookie": cookie 
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Cookie": cookie,
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         videos_info = response.json()
-        if(videos_info['code'] != 0):
-            print(videos_info['message'])
+        if videos_info["code"] != 0:
+            print(videos_info["message"])
             return
     else:
         print(f"请求失败，状态码：{response.status_code}")
-    #获取最新一期的数据，page即为期数
-    page = videos_info['data']['list'][0]['number']
-    #res为最终的结果
+    # 获取最新一期的数据，page即为期数
+    page = videos_info["data"]["list"][0]["number"]
+    # res为最终的结果
     res = []
     count = 1
-    
 
-    while(count <= len):
+    while count <= len:
         url = "https://api.bilibili.com/x/web-interface/popular/series/one?number="
         str_page = str(page)
         url += str_page
@@ -35,48 +34,52 @@ def get_hot_data(cookie, len):
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             videos_info = response.json()
-            if(videos_info['code'] != 0):
-                print(videos_info['message'])
+            if videos_info["code"] != 0:
+                print(videos_info["message"])
                 return
         else:
             print(f"请求失败，状态码：{response.status_code}")
-        
-        for video_info in videos_info['data']['list'] :
+
+        for video_info in videos_info["data"]["list"]:
             sigle_res = {}
-            sigle_res["bvid"] = video_info['bvid']
-            sigle_res["title"] = video_info['title']
+            sigle_res["bvid"] = video_info["bvid"]
+            sigle_res["title"] = video_info["title"]
             sigle_res["pic"] = video_info["pic"]
-            sigle_res['author'] = video_info['owner']['name']
-            sigle_res["view"] = video_info["stat"]['view']
-            sigle_res["like"] = video_info["stat"]['like']
-            sigle_res["favorite"] = video_info["stat"]['favorite']
-            sigle_res["coin"] = video_info["stat"]['coin']
-            sigle_res["share"] = video_info["stat"]['share']
+            sigle_res["author"] = video_info["owner"]["name"]
+            sigle_res["view"] = video_info["stat"]["view"]
+            sigle_res["like"] = video_info["stat"]["like"]
+            sigle_res["favorite"] = video_info["stat"]["favorite"]
+            sigle_res["coin"] = video_info["stat"]["coin"]
+            sigle_res["share"] = video_info["stat"]["share"]
             sigle_res["duration"] = video_info["duration"]
             # tag比价麻烦，需要单独去获取详细信息
-            url_2 = "https://api.bilibili.com/x/web-interface/view/detail?bvid=" + video_info['bvid']
+            url_2 = (
+                "https://api.bilibili.com/x/web-interface/view/detail?bvid="
+                + video_info["bvid"]
+            )
             response = requests.get(url_2, headers=headers)
             if response.status_code == 200:
                 video_detail = response.json()
-                if(video_detail['code'] != 0):
-                    print(video_detail['message'])
-                    return      
+                if video_detail["code"] != 0:
+                    print(video_detail["message"])
+                    return
             else:
-                print(f"请求失败，状态码：{response.status_code}")      
-            #print(video_detail['data']['Tags'])
-            sigle_res['tag'] = [tag['tag_name'] for tag in video_detail['data']['Tags']]
+                print(f"请求失败，状态码：{response.status_code}")
+            # print(video_detail['data']['Tags'])
+            sigle_res["tag"] = [tag["tag_name"] for tag in video_detail["data"]["Tags"]]
 
             res.append(sigle_res)
-            print(video_info['bvid'], count)
-            count+=1
-            if(count > len):
+            print(video_info["bvid"], count)
+            count += 1
+            if count > len:
                 break
-        page-=1 
-        if(page == 0):
+        page -= 1
+        if page == 0:
             break
-        with open('hotVideo.json', 'w', encoding='utf-8') as json_file:
-        # 使用 json.dump() 将字典写入文件
-            json.dump(res, json_file, indent=4,ensure_ascii=False)  # indent=4 用来让输出格式更易读 
-   
-    return res
+        with open("hotVideo.json", "w", encoding="utf-8") as json_file:
+            # 使用 json.dump() 将字典写入文件
+            json.dump(
+                res, json_file, indent=4, ensure_ascii=False
+            )  # indent=4 用来让输出格式更易读
 
+    return res
